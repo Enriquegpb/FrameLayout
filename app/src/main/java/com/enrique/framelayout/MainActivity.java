0f66ebf77d0e47775bf0aaef016a7d7a266bc276
Enrique <fpemlktz@gmail.com>
2021-11-12 12:49:15 +0100

    Final FrameLayout

--- app/src/main/java/com/enrique/framelayout/MainActivity.java
+++ app/src/main/java/com/enrique/framelayout/MainActivity.java
@@ -3,12 +3,25 @@ package com.enrique.framelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    ImageView Iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.button);
+        Iv=(ImageView) findViewById(R.id.imageView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button.setVisibility(View.INVISIBLE);
+                Iv.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
